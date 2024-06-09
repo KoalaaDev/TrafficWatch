6161a7abdf0ddea2ec787ec279164a2754c0def5
KoalaaDev <Williamhow219@hotmail.com>
2024-06-09 20:26:44 +0400

    fixed train.py not working

--- train.py
+++ train.py
@@ -1,9 +1,7 @@
 from ultralytics import YOLO
+if __name__ == "__main__":
+    # Load the model
+    model = YOLO("yolov8m.pt")  # build from YAML and transfer weights
 
-# Load a model
-model = YOLO("yolov8n.yaml")  # build a new model from YAML
-model = YOLO("yolov8m.pt")  # load a pretrained model (recommended for training)
-model = YOLO("yolov8m.yaml").load("yolov8m.pt")  # build from YAML and transfer weights
-
-# Train the model
-results = model.train(data="data.yaml", epochs=100, imgsz=640)
+    # Train the model
+    results = model.train(data="data.yaml", epochs=100, imgsz=640)
